90a64f00ed15b5d00f52b15cbad0b7dca99fe6c3
siunix <abd.siunix@gmail.com>
2022-02-11 22:22:11 +0700

    add header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Autocomplete } from '@react-google-maps/api'
+import { AppBar, Toolbar, Typography, Box } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+import { Search, SearchIconWrapper, StyledInputBase } from './styles'
 
 function Header() {
   return (
-    <div>Header</div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          TRIP ADVISOR
+        </Typography>
+        <Box display='flex'>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Explore New Place"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
